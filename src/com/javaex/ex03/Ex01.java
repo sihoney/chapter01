1fcbf220ba79179c38618e657635aab24541c4b8
sihoney <hwangheewon@naver.com>
2021-11-15 17:11:41 +0900

    if-else

---
+++ src/com/javaex/ex03/Ex01.java
@@ -0,0 +1,20 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("점수: ");
+		int a = sc.nextInt();
+		
+		if(a >= 60) {
+			System.out.println("합격입니다. ");
+		} else {
+			System.out.println("불합격입니다");
+		}
+		
+		sc.close();
+	}
+}
